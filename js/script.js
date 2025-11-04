1a8a33788bc42e742c87f756deaa1438d71bcb16
Sebbbenator <eaa25ssg@students.eaaa.dk>
2025-11-04 10:47:58 +0100

    done?

--- js/script.js
+++ js/script.js
@@ -31,35 +31,39 @@ if (sectionOne) {
   });
 }
 
-
 const simon = document.getElementById("simon");
 const piger = document.getElementById("piger");
 const drenge = document.getElementById("drenge");
 
-// if (simon) {
- // const soundSimon = new Audio("sound/simon.mp3");
- // simon.addEventListener("click", function () {
-  //  soundSimon.play();
- // });
-// }
 
-if (piger) {
-  const soundPiger = new Audio("sound/piger.mp3");
-  piger.addEventListener("click", function () {
-    soundPiger.play();
-  });
-}
 
-if (drenge) {
-  const soundDrenge = new Audio("sound/drenge.mp3");
-  drenge.addEventListener("click", function () {
-    soundDrenge.play();
-  });
-}
+const soundDrenge = new Audio("sound/drenge.mp3");
+drenge.addEventListener("click", () => {
+  soundDrenge.play();
+  drenge.classList.remove("animate");
+  drenge.classList.toggle("anim-scale");
+});
+
+const soundPiger = new Audio("sound/piger.mp3");
+piger.addEventListener("click", () => {
+  soundPiger.play();
+  piger.classList.remove("animate");
+  piger.classList.toggle("anim-scale");
+});
+
 const soundSimon = new Audio("sound/simon.mp3");
-  simon.addEventListener("click", () => {
-    soundSimon.play();
-    simon.classList.remove("animate");
-    simon.classList.toggle("anim-scale");
+simon.addEventListener("click", () => {
+  soundSimon.play();
+  simon.classList.remove("animate");
+  simon.classList.toggle("anim-scale");
+});
+
+const gamle = document.getElementById("gamlesen");
+if (gamle) {
+  gamle.addEventListener("click", function () {
+    this.classList.toggle("anim-bounce");
+    gamle.classList.toogle("animate");
+    gamle.classList.toogle("anim-scale");
   });
+}
 
